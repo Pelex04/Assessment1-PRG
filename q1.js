b5deded8a281513090ea3998e18aa874d5d5abc1
pelex04 <naomibenjamin738@gmail.com>
2025-08-09 18:11:51 +0200

    changes outside the exams, made for proffessionalism

--- q1.js
+++ q1.js
@@ -1,12 +1,19 @@
 let num = 1;
 
-let name = " person";
+let name2 = " person";
 
 const object = {
     name: " person"
 }
 class Person2 {
+    constructor(){
+        
+    }
 
 }
 
 let one = 'c';
+console.log(typeof Person2)
+console.log(typeof object)
+console.log(typeof name2)
+console.log(typeof num)
